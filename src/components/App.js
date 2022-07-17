6d3ea1de98e304f753ecb993471adea60d4ce675
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-07-17 12:10:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
   return (
-    <div id="main">
-      <img src={star}  height="300px" width="300px" />
-    </div>
+    <>
+       <div id="main">
+           <img src={star}  height="300px" width="300px" />
+       </div>
+    </>
   )
 }
 
