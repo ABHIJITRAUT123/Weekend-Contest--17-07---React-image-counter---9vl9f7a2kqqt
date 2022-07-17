9f9065497a0e6e82f5995009879beb11992a4e0e
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-07-17 12:22:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,16 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  
+  const handleClick1 = () => {
+  setCounter(counter+1)
+  }
+  
+   const handleClick2 = () => {
+  setCounter(counter-1)
+  }
+  
   return (
     <>
        <div id="main">
